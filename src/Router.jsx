2765b546ed2f4032c57d48d62ed9c45d3d47b43a
Pedro Hernandez <969572632.max@gmail.com>
2024-01-21 14:35:46 -0500

    vista2 modificado

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import Country from "./components/Country";
 import Vista1 from "./components/Vista1";
-import Vista2 from "./components/vista2";
+import Vista2 from "./components/Vista2";
 
 export default function AppRouter() {
   return (
